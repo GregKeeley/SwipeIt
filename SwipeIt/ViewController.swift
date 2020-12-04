7b947b959201221b39452811350656b3283f0422
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-12-04 14:50:06 -0500

    Initial Commit

---
+++ SwipeIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeIt
+//
+//  Created by Gregory Keeley on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
