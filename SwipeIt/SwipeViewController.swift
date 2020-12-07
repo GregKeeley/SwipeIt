5ae4d0e94ec4119e5d3087c1db0003b5a9a04f17
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-12-07 17:12:01 -0500

    - Added basic moving view

--- SwipeIt/SwipeViewController.swift
+++ SwipeIt/SwipeViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class SwipeViewController: UIViewController {
 
+    @IBOutlet weak var swipableView: UIView!
+    @IBOutlet weak var viewToMove: UIView!
+    
+    var location = CGPoint(x:0, y:0)
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moveView(touches)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moveView(touches)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    @IBAction func viewSwiped(_ sender: UIPanGestureRecognizer) {
-        print("Number of touches: \(sender.numberOfTouches)")
-        print("State: \(sender.state.rawValue)")
+    private func moveView(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else {
+            print("Something went wrong with the touching")
+            return
+        }
+        location = touch.location(in: self.view)
+        if viewToMove.frame.origin.x > 0 && viewToMove.frame.origin.x < view.frame.width {
+            viewToMove.center.x = location.x
+        } else {
+            
+        }
     }
+    @IBAction func viewSwiped(_ sender: UIPanGestureRecognizer) {
+
+    }
+    
 }
