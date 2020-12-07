2bb24b5ba050b6d7a8da980390e178e4998d5461
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-12-07 17:29:28 -0500

    Updated move functions

--- SwipeIt/SwipeViewController.swift
+++ SwipeIt/SwipeViewController.swift
@@ -13,7 +13,8 @@ class SwipeViewController: UIViewController {
     @IBOutlet weak var viewToMove: UIView!
     
     var location = CGPoint(x:0, y:0)
-
+    var originalViewLocation = CGPoint(x:0, y: 0)
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         moveView(touches)
     }
@@ -26,14 +27,18 @@ class SwipeViewController: UIViewController {
     }
     private func moveView(_ touches: Set<UITouch>) {
         guard let touch = touches.first else {
-            print("Something went wrong with the touching")
             return
         }
+        originalViewLocation = location
         location = touch.location(in: self.view)
-        if viewToMove.frame.origin.x > 0 && viewToMove.frame.origin.x < view.frame.width {
-            viewToMove.center.x = location.x
+        if location.x > viewToMove.frame.origin.x {
+            if viewToMove.center.x > 0 && viewToMove.center.x < view.frame.width {
+                viewToMove.center.x = location.x
+            } else {
+                viewToMove.center.x = originalViewLocation.x
+            }
         } else {
-            
+            viewToMove.center.x = originalViewLocation.x
         }
     }
     @IBAction func viewSwiped(_ sender: UIPanGestureRecognizer) {
