61c4e2e2aba8d49cd8cfe5a4988e2c8680a398fb
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-12-15 18:15:20 -0500

    - Pushing progress, button on view now resets after touches end, or if the button goes off screen

--- SwipeIt/SwipeButtonViewController.swift
+++ SwipeIt/SwipeButtonViewController.swift
@@ -7,18 +7,74 @@
 
 import UIKit
 
+// Notes: Pan gesture seems unecessary to make this work (the view, in this case the UIButton that needs to move)
+// Registering the touches is the crucial part, then we can move what we need,
+
 class SwipeButtonViewController: UIViewController {
 
     @IBOutlet weak var swipeButtonToMove: UIButton!
     
+    var location = CGPoint(x:0, y:0)
+    var minViewCoords = CGPoint(x: 0, y: 0) {
+        didSet {
+            print("\(minViewCoords.x)")
+        }
+    }
+    var maxViewCoords = CGPoint(x: 0, y: 0) {
+        didSet {
+            print("\(maxViewCoords.x)")
+        }
+    }
+    var touches: Set<UITouch>?
+     //MARK:- Override touches funcs
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.touches = touches
+        moveViewWithTouch(touches)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.touches = touches
+        moveViewWithTouch(touches)
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.touches?.removeAll()
+        // After touches end, what do I want to do here?
+        print("Touches ended")
+        resetButtonToOriginLocation()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-    }
-    func configureUI() {
-        swipeButtonToMove.layer.cornerRadius = swipeButtonToMove.frame.width / 2
-        view.layer.cornerRadius = view.frame.height / 2
-        self.view.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        loadInformation()
     }
     
+    func configureUI() {
+        swipeButtonToMove.layer.cornerRadius = swipeButtonToMove.frame.width / 2
+        self.view.layer.cornerRadius = view.frame.height / 2
+    }
+    func loadInformation() {
+        minViewCoords.x = view.frame.minX + 4
+        maxViewCoords.x = view.frame.maxX - 4
+    }
+    private func resetButtonToOriginLocation() {
+        swipeButtonToMove.frame.origin.x = minViewCoords.x
+    }
+    private func moveViewWithTouch(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else {
+            return
+        }
+        location = touch.location(in: self.view)
+        if location.x > minViewCoords.x && location.x < maxViewCoords.x {
+            if swipeButtonToMove.frame.minX >= minViewCoords.x && swipeButtonToMove.frame.maxX <= maxViewCoords.x {
+                swipeButtonToMove.center.x = location.x
+            } else {
+                resetButtonToOriginLocation()
+            }
+        } else {
+            resetButtonToOriginLocation()
+        }
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let touches = self.touches else { return }
+        moveViewWithTouch(touches)
+    }
 }
--- SwipeIt/ViewController.swift
+++ SwipeIt/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-    
+    func configureUI() {
+        buttonContainerView.layer.masksToBounds = true
+        buttonContainerView.layer.cornerRadius = buttonContainerView.frame.height / 2
+    }
 }
 
